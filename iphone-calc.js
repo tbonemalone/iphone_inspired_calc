43736b23a462be53ef2850d6e5405da164473af2
tbonemalone <tony.org@gmail.com>
2011-09-03 17:07:34 -0700

    Added ids to buttons

---
+++ iphone-calc.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	console.log("ready");
+	var display = []
+	
+	function getNum() {
+		var i = $(this).text();
+		console.log("i = " + i);
+	}
+	
+	
+	if()
+}); //end doc ready
