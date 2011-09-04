d30757608ec63629addc330194bd75d1e0c33e4c
tbonemalone <tony.org@gmail.com>
2011-09-03 19:51:00 -0700

    Clicking numbers now makes them show in display
    Began adding js to make the calc work.

--- iphone-calc.js
+++ iphone-calc.js
@@ -1,12 +1,38 @@
 $(document).ready(function() {
 	console.log("ready");
-	var display = []
+	var num = ""	
 	
 	function getNum() {
-		var i = $(this).text();
-		console.log("i = " + i);
+		console.log("num = " + num);
+		var i = $(this).text()
+		num = num + i;
+		console.log("num = " + num);
+		$("#display").val(num);
 	}
 	
+	function clearAll() {
+		num = "";
+	}
 	
-	if()
+	function switchNegPos() {
+		if (num === 0) {
+			num = num
+		}
+		else if (num > 0) {
+			num = num - (num * 2);
+		}
+		
+		else {
+			num = num + (num * 2);
+		}
+	}
+
+	$(".num").click(function() {
+		console.log("num = " + num);
+		var i = $(this).text()
+		num = num + i;
+		console.log("num = " + num);
+		$("#display").val(num);
+	});
 }); //end doc ready
+
